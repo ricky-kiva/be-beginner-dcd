4cc76f5f1d2db84dd7ed46bafabef8f936ee1be0
ricky-kiva <ricky.kiva@gmail.com>
2023-10-06 12:33:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// REPL: Read-Eval-Print-Loop, a way to execute 1 line of JS code using Node.js
+// - access Node.js "REPL" by writing `node` in the console
+// - do multiline REPL by writing `.editor`
+
+// execute this file using `node ${filename}` (node index.js)
+const message = (name) => {
+    console.log(`Good day ${name} !`)
+}
+
+message('Rickyslash')
