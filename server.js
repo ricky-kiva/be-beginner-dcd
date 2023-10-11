c12b57f2c5885fe95935218b0f16c417ae0c1ff0
ricky-kiva <ricky.kiva@gmail.com>
2023-10-11 22:43:46 +0700

    feat(server): add first http server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("We are going to make server boys")
+const http = require('http')
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html')
+
+    response.statusCode = 200
+    response.end('<h1>Hello on HTTP Server boys!</h1>')
+}
+
+const server = http.createServer(requestListener)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+})
