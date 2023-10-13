08100b080dc10cfeda254d70c087912cbdbccd73
ricky-kiva <ricky.kiva@gmail.com>
2023-10-13 23:25:57 +0700

    feat(server): enable CORS on all path

--- notes-app/src/server.js
+++ notes-app/src/server.js
@@ -4,7 +4,12 @@ const routes = require('./routes')
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'] // apply CORS on all path (add in each path on 'options' if want it to be specific)
+            }
+        }
     })
 
     server.route(routes)
