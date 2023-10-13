902d482d90c269bf6d98a99386a51fd8a8220e88
ricky-kiva <ricky.kiva@gmail.com>
2023-10-13 13:19:55 +0700

    feat(routes): route /hello with path parameter

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -23,6 +23,14 @@ const routes = [{
         return "Can't access the page with that method"
     }
 }, {
+    method: 'GET',
+    path: '/hello/{name?}', // `?` states that the parameter is optional
+    // optional parameter could only be obtained from the last path
+    handler: (request, h) => {
+        const { name = 'Human'} = request.params // get parameter from path
+        return `Hello, ${name}`
+    }
+}, {
     method: "*",
     path: '/{any*}',
     handler: (request, h) => {
