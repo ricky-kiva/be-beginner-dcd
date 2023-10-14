0cef96e3f7d3ac957c0090d8cec8ce931f8c5d3c
ricky-kiva <ricky.kiva@gmail.com>
2023-10-14 19:29:41 +0700

    feat(server): add logic to run on cloud server

--- notes-app/src/server.js
+++ notes-app/src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'] // apply CORS on all path (add in each path on 'options' if want it to be specific)
